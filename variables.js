29f16a0d67c30b990f82325cfcb162f41c800d1f
Prakarsh4 <prakarshkumar819@gmail.com>
2026-06-25 16:31:20 +0530

    variables learned

---
+++ variables.js
@@ -0,0 +1,7 @@
+const AccountID="your_account_id_here";// cant be changed
+var APIKey="your_api_key_here";// can be changed
+let APISecret="your_api_secret_here";// can be changed
+const accountID = "your_account_id_not_changed";// cant be changed
+  console.log("Account ID:", accountID);
+  //table ke form me output ayega[] isse use krne se
+  console.table({ AccountID, APIKey, APISecret, accountID });
