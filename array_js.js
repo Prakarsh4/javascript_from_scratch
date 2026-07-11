15fcd87a420782ff2985cf6d18442d7247b2ab16
Prakarsh4 <prakarshkumar819@gmail.com>
2026-07-12 00:31:42 +0530

    multiple array in single array

--- array_js.js
+++ array_js.js
@@ -1,4 +1,4 @@
-const marvel_heroes=[
+/*const marvel_heroes=[
     "Iron Man",
     "Captain America",]
     const dc_heroes=[
@@ -8,3 +8,7 @@ const marvel_heroes=[
 ]
 const all_heroes=marvel_heroes.concat(dc_heroes)// combine dc_heroes as all new element. not treated as individiual element of dc heroes. all the dc heroes qill be single new element.
 const all_heroes2=[...marvel_heroes,...dc_heroes]// combine dc_heroes as individual element. all the dc heroes will be treated as individual element of all_heroes2.
+*/
+const real_numbers=[1,2,3,[4,5,6],[7,8,9],[10,11,12,[13,14,15]]]
+const flat_numbers=real_numbers.flat(Infinity)// flattens the array to a depth of 2. so the nested arrays will be flattened to a single array.
+console.log(flat_numbers)
